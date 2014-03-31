47484e638799e558482ec4537fe37e305cb06cae
Leeroy Brun <leeroy.brun@gmail.com>
2014-03-31 21:06:53 +0200

    Continue

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var Downloader = require('./Downloader');
+var Downloader = require('./lib/Downloader');
 var prompt = require('prompt');
 
 prompt.start();
 		
 prompt.get(['url'], function (err, result) {
-	var downloader = new Downloader(result);
-	downloader.start();
+	var downloader = new Downloader();
+
+	downloader.start(result.url);
 
 	downloader.on('error', function(error) {
 		console.error(error);
